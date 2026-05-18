b4d8292c92f6a61c708ec4ad69f160b28b33cbb7
tulsi <tulasikodi5@gmail.com>
2026-05-18 16:04:19 +0530

    i have added the correct product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,69 +4,53 @@ from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 
-class ProductCategoryPage(BasePage):
+class ProductPage(BasePage):
 
-    # Locators
-    PHONES_CATEGORY = (By.LINK_TEXT, "Phones")
-    LAPTOPS_CATEGORY = (By.LINK_TEXT, "Laptops")
-    MONITORS_CATEGORY = (By.LINK_TEXT, "Monitors")
-    PRODUCT_CARDS = (By.CSS_SELECTOR, "div.card-block")
-    PRODUCT_TITLES = (By.CSS_SELECTOR, "div.card-block h4 a")
+    phones_category = (By.LINK_TEXT, "Phones")
+    laptops_category = (By.LINK_TEXT, "Laptops")
+    monitors_category = (By.LINK_TEXT, "Monitors")
+    product_cards = (By.CSS_SELECTOR, "div.card-block")
+    product_titles = (By.CSS_SELECTOR, "div.card-block h4 a")
 
     def __init__(self, driver):
         super().__init__(driver)
+        self.wait = WebDriverWait(self.driver, 10)
 
     def open(self):
-        """Opens the Demoblaze homepage using base_page open_url."""
         self.open_url()
 
     def click_phones_category(self):
-        """Clicks on Phones category."""
-        wait = WebDriverWait(self.driver, 10)
-        phones = wait.until(
-            EC.element_to_be_clickable(self.PHONES_CATEGORY)
-        )
-        phones.click()
+        self.wait.until(
+            EC.element_to_be_clickable(self.phones_category)
+        ).click()
 
     def click_laptops_category(self):
-        """Clicks on Laptops category."""
-        wait = WebDriverWait(self.driver, 10)
-        laptops = wait.until(
-            EC.element_to_be_clickable(self.LAPTOPS_CATEGORY)
-        )
-        laptops.click()
+        self.wait.until(
+            EC.element_to_be_clickable(self.laptops_category)
+        ).click()
 
     def click_monitors_category(self):
-        """Clicks on Monitors category."""
-        wait = WebDriverWait(self.driver, 10)
-        monitors = wait.until(
-            EC.element_to_be_clickable(self.MONITORS_CATEGORY)
-        )
-        monitors.click()
+        self.wait.until(
+            EC.element_to_be_clickable(self.monitors_category)
+        ).click()
 
     def get_product_titles(self):
-        """Returns list of all visible product titles."""
-        wait = WebDriverWait(self.driver, 10)
-        elements = wait.until(
-            EC.presence_of_all_elements_located(self.PRODUCT_TITLES)
+        elements = self.wait.until(
+            EC.presence_of_all_elements_located(self.product_titles)
         )
         return [element.text for element in elements]
 
     def get_product_count(self):
-        """Returns number of products displayed."""
-        wait = WebDriverWait(self.driver, 10)
-        products = wait.until(
-            EC.presence_of_all_elements_located(self.PRODUCT_CARDS)
+        products = self.wait.until(
+            EC.presence_of_all_elements_located(self.product_cards)
         )
         return len(products)
 
     def is_products_displayed(self):
-        """Returns True if products are visible on page."""
         try:
-            wait = WebDriverWait(self.driver, 10)
-            elements = wait.until(
-                EC.presence_of_all_elements_located(self.PRODUCT_CARDS)
+            products = self.wait.until(
+                EC.presence_of_all_elements_located(self.product_cards)
             )
-            return len(elements) > 0
-        except:
+            return len(products) > 0
+        except Exception:
             return False
