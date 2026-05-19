4198940018d60a9a9c9fb595b2f3d48e3b2d1033
Kripal Krishna <krihsnakripal.dev@gmail>
2026-05-19 12:34:54 +0530

    updated conftest

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
@@ -10,3 +12,14 @@ def driver():
     yield driver
 
     driver.quit()
+
+
+def wait(driver):
+    return WebDriverWait(driver, 10)
+
+
+def accept_alert(driver):
+    alert = wait(driver).until(EC.alert_is_present())
+    text = alert.text
+    alert.accept()
+    return text
